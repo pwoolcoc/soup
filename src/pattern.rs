4d0bd7dca395a6d2c42efbf52ce7962392361532
Paul Woolcock <paul@woolcock.us>
2018-11-05 07:14:06 -0500

    d'oh, forgot to stage this

---
+++ src/pattern.rs
@@ -0,0 +1,51 @@
+//! A trait used to indicate a type which can be used to match a value
+//!
+//! Any type that implements this trait can be passed to the various QueryBuilder methods in order
+//! to match an element
+//!
+//! # Example
+//!
+//! ```rust
+//! # extern crate soup;
+//! use soup::prelude::*;
+//! use soup::pattern::Pattern;
+//!
+//! struct MyType(String);
+//!
+//! impl Pattern for MyType {
+//!     fn matches(&self, haystack: &str) -> bool {
+//!         self.0.matches(haystack)
+//!     }
+//! }
+//!
+//! let soup = Soup::new(r#"<div id="foo"></div>"#);
+//! let result = soup.tag(MyType("div".to_string()))
+//!                  .find()
+//!                  .unwrap();
+//! assert_eq!(result.get("id").unwrap(), "foo".to_string());
+//! ```
+#[cfg(feature = "regex")]
+use regex::Regex;
+
+pub trait Pattern {
+    fn matches(&self, haystack: &str) -> bool;
+}
+
+impl<'a> Pattern for &'a str {
+    fn matches(&self, haystack: &str) -> bool {
+        *self == haystack
+    }
+}
+
+impl Pattern for String {
+    fn matches(&self, haystack: &str) -> bool {
+        self == haystack
+    }
+}
+
+#[cfg(feature = "regex")]
+impl Pattern for Regex {
+    fn matches(&self, haystack: &str) -> bool {
+        self.is_match(haystack)
+    }
+}
