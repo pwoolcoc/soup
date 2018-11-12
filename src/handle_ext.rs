827d0395126c3684cea65bd1404d95a7a8da50f9
Paul Woolcock <paul@woolcock.us>
2018-11-12 11:04:36 -0500

    Whoops, forgot to commit this

---
+++ src/handle_ext.rs
@@ -0,0 +1,79 @@
+use html5ever::rcdom::Handle;
+
+use crate::{
+    find::{AttrQuery, QueryBuilder, QueryWrapper, TagQuery},
+    pattern::Pattern,
+};
+
+/// Adds the QueryBuilder constructor methods to the `html5ever::rcdom::Node` type
+pub trait HandleExt {
+    /// Retrieves the Handle that these methods will work on
+    fn get_handle(&self) -> Handle;
+
+    // QueryBuilder constructor methods
+    /// Starts building a Query, with limit `limit`
+    fn limit<'a>(&self, limit: usize) -> QueryBuilder<'a, (), ()> {
+        let handle = self.get_handle();
+        let qb = QueryBuilder::new(handle);
+        qb.limit(limit)
+    }
+
+    /// Starts building a Query, with tag `tag`
+    fn tag<'a, P: Pattern>(
+        &self,
+        tag: P,
+    ) -> QueryBuilder<'a, TagQuery<P>, QueryWrapper<'a, (), ()>> {
+        let handle = self.get_handle();
+        let qb = QueryBuilder::new(handle);
+        qb.tag(tag)
+    }
+
+    /// Starts building a Query, with attr name `name`
+    fn attr_name<'a, P>(&self, name: P) -> QueryBuilder<'a, AttrQuery<P, bool>, QueryWrapper<'a, (), ()>>
+    where
+        P: Pattern
+    {
+        let handle = self.get_handle();
+        let qb = QueryBuilder::new(handle);
+        qb.attr_name(name)
+    }
+
+    /// Starts building a Query, with attr `attr`
+    fn attr<'a, P, Q>(
+        &self,
+        name: P,
+        value: Q,
+    ) -> QueryBuilder<'a, AttrQuery<P, Q>, QueryWrapper<'a, (), ()>>
+    where
+        P: Pattern,
+        Q: Pattern,
+    {
+        let handle = self.get_handle();
+        let qb = QueryBuilder::new(handle);
+        qb.attr(name, value)
+    }
+
+    /// Starts building a Query, with class `class`
+    fn class<'a, P: Pattern>(
+        &self,
+        value: P,
+    ) -> QueryBuilder<'a, AttrQuery<&'static str, P>, QueryWrapper<'a, (), ()>> {
+        let handle = self.get_handle();
+        let qb = QueryBuilder::new(handle);
+        qb.class(value)
+    }
+
+    /// Starts building a Query, with recursion set to `recursive`
+    fn recursive<'a>(&self, recursive: bool) -> QueryBuilder<'a, (), ()> {
+        let handle = self.get_handle();
+        let qb = QueryBuilder::new(handle);
+        qb.recursive(recursive)
+    }
+
+}
+
+impl HandleExt for Handle {
+    fn get_handle(&self) -> Handle {
+        self.clone()
+    }
+}
