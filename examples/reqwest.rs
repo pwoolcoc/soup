ff56bedee98e30935019f45e13619679ba794b09
Paul Woolcock <paul@woolcock.us>
2020-02-14 13:48:47 -0500

    Update example to use `dyn Trait`

--- examples/reqwest.rs
+++ examples/reqwest.rs
@@ -4,7 +4,7 @@ extern crate soup;
 use soup::prelude::*;
 use std::error::Error;
 
-fn main() -> Result<(), Box<Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let resp = reqwest::get("https://docs.rs/soup/0.1.0/soup/")?;
     let soup = Soup::from_reader(resp)?;
     let result = soup
